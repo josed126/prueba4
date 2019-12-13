46c41a54d97f894b9dc90690600e83906b2918d6
josed126 <clavellino1993@gmail.com>
2019-12-13 15:20:55 +0100

    prueba2

--- DividirPalabras.java
+++ DividirPalabras.java
@@ -2,9 +2,9 @@ public class DividirPalabras {
     public static void main (String[] args){
         int space = args.toString();
         
-        String first2 = args.substring(space + 1);
+        String first3 = args.substring(space + 1);
         String first = args.substring(0,1);
-        String Total = first + ", " + first2;
+        String Total = first + ", " + first3;
         return Total;
     }
 }
