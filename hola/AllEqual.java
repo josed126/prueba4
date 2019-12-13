d0af49a4385df628e9a11749b016bdb19b48ad69
josed126 <clavellino1993@gmail.com>
2019-12-13 15:18:51 +0100

    prueba

---
+++ hola/AllEqual.java
@@ -0,0 +1,17 @@
+public class AllEqual
+{
+    public static void main(String[] args)
+    {
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+	int c = Integer.parseInt(args[2]);
+
+	if (a == b && a == c && c == b) {
+		 System.out.println("equal");}
+	else {
+		 System.out.println("not equal");
+}
+
+}  
+    
+}
